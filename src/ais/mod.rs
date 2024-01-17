e51f97dfea6e4ba60cbd097dbee0555cfeb33d3b
GYoung <ytok1108@aol.com>
2024-01-17 23:29:00 +0900

    Learn_Rust_OpenAI_API_Jeremy__Chone__Hello World_OPEN API KEY

--- src/ais/mod.rs
+++ src/ais/mod.rs
@@ -4,13 +4,19 @@ use async_openai::Client;
 
 use crate::Result;
 
-use self::asst::{OaClient, ENV_OPENAI_API_KEY};
+use async_openai::config::OpenAIConfig;
+
+// region : --- Client
+pub const ENV_OPENAI_API_KEY: &str = "OPENAI_API_KEY";
+
+pub type OaClient = Client<OpenAIConfig>;
 
 pub fn new_oa_client() -> Result<OaClient> {
     if std::env::var(ENV_OPENAI_API_KEY).is_ok() {
         Ok(Client::new())
     } else {
         println!("No {ENV_OPENAI_API_KEY} env variable. Please set it.");
+
         Err("No openai api key in env".into())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,18 @@ mod ais;
 mod buddy;
 mod error;
 
-use crate::ais::asst::ENV_OPENAI_API_KEY;
+use crate::ais::new_oa_client;
 
 pub use self::error::{Error, Result};
 
 async fn start() -> Result<()> {
-    println!("Hello, world!");
+    let oac = new_oa_client()?;
+    println!("-->> oac: {oac:?}");
     Ok(())
 }
 #[tokio::main]
 async fn main() {
     println!();
-    println!("API Key test {}", ENV_OPENAI_API_KEY);
 
     match start().await {
         Ok(_) => println!("\nBye!\n"),
